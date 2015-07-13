a1be961b81f46d9b95688bf0518f2aa21fae07df
pablocastilla <pablo.castilla@gmail.com>
2015-07-13 11:29:44 +0200

    DeviceActor created

---
+++ Shared/Actors/Device.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Persistence;
+
+namespace Shared.Actors
+{
+    public class Device : AtLeastOnceDeliveryActor 
+    {
+
+        string serialNumber;
+
+        public Device(string serialNumber)
+        {
+            this.serialNumber = serialNumber;
+        }
+
+        public override string PersistenceId
+        {
+            get { return serialNumber; }
+        }
+
+        protected override bool ReceiveCommand(object message)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        protected override bool ReceiveRecover(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
