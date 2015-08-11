1cda3c39c7d65d9e5980cd6af6b8472dbd4eb085
pablocastilla <pablo.castilla@gmail.com>
2015-08-11 17:42:13 +0200

    switch actor created

---
+++ Shared/Actors/SwitchProcess.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Actors
+{
+    public class SwitchProcess : PersistentActor
+    {
+    }
+}
